3650b41a673559801365ef33787f978c66abdc71
Pavel Egorov <nxblnd@desktop.localdomain>
2024-10-28 23:37:29 +0300

    Add telescope keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,9 +14,21 @@ keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move to lower split' })
 keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move to upper split' })
 keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move to right split' })
 
--- Git keymaps
+-- Neogit keymaps
 keymap.set('n', '<Leader>g', ':Neogit<cr>', { desc = 'Open Neogit' })
 keymap.set('n', '<Leader>Gl', ':Neogit log<cr>b<cr>', { desc = 'Open Neogit log' })
+
+-- Telescope keymaps
+local t_builtin = require('telescope.builtin')
+if t_builtin then
+    keymap.set('n', '<Leader>sh', t_builtin.help_tags, { desc = 'Search help' })
+    keymap.set('n', '<Leader>sf', t_builtin.find_files, { desc = 'Search files' })
+    keymap.set('n', '<Leader>sg', t_builtin.live_grep, { desc = 'Search by grep' })
+    keymap.set('n', '<Leader>sw', t_builtin.grep_string, { desc = 'Search word under cursor' })
+    keymap.set('n', '<Leader>sr', t_builtin.resume, { desc = 'Search resume' })
+    keymap.set('n', '<Leader>so', t_builtin.oldfiles, { desc = 'Search previously open files' })
+    keymap.set('n', '<Leader><Leader>', t_builtin.buffers, { desc = 'Search open buffers' })
+end
 
 -- Misc keymaps
 keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -2,7 +2,9 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         dependencies = {
-            'nvim-lua/plenary.nvim',
+            { 'nvim-lua/plenary.nvim' },
+            { 'nvim-telescope/telescope-ui-select.nvim' },
+            { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
         },
     },
     {
