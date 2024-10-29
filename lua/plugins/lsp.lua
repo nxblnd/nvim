a5424d6cdf54fc11892a0603796be3efd86e3644
Pavel Egorov <nxblnd@desktop.localdomain>
2024-10-30 02:52:22 +0300

    Improve lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,14 +27,88 @@ return {
     {
         'neovim/nvim-lspconfig',
         dependencies = {
-            'williamboman/mason.nvim',
+            {
+                'williamboman/mason.nvim',
+                build = ':MasonUpdate',
+                config = true,
+            },
             'williamboman/mason-lspconfig.nvim',
+            {
+                'j-hui/fidget.nvim',
+                opts = {},
+            },
         },
-    },
-    {
-        'williamboman/mason.nvim',
-        build = ':MasonUpdate',
-        opts = {},
+        config = function()
+            local function create_keymaps(event_buffer)
+                local keymap = function(kmap, func, desc, mode)
+                    keymap_generic = require('../config/keymaps.lua')
+                    keymap_generic(kmap, func, desc, { mode = mode or 'n', buffer = event_buffer })
+                end
+                t_builtin = require('telescope.builtin')
+
+                keymap('gd', t_builtin.lsp_definitions, 'Goto definition')
+                keymap('gD', vim.lsp.buf.declaration, 'Goto declaration')
+                keymap('gr', t_builtin.lsp_references, 'Goto references')
+                keymap('gI', t_builtin.lsp_implementations, 'Goto implementation')
+                keymap('g<Leader>t', t_builtin.lsp_type_definitions, 'Goto type definition')
+                keymap('<Leader>sc', t_builtin.lsp_document_symbols, 'Search symbols in document')
+                keymap('<Leader>sC', t_builtin.lsp_workspace_symbols, 'Search symbols in workspace')
+                keymap('<Leader>cn', vim.lsp.buf.rename, 'Rename under cursor with LSP')
+                keymap('<Leader>a', vim.lsp.buf.code_action, 'Code action', { mode = { 'n', 'x' } })
+            end
+
+            local function hide_highlight(event_buffer)
+                local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+
+                vim.api.nvim_create_autocmd(
+                    { 'CursorHold', 'CursorHoldI' },
+                    {
+                        buffer = event_buffer,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+
+                vim.api.nvim_create_autocmd(
+                    { 'CursorMoved', 'CursorMovedI' },
+                    {
+                        buffer = event_buffer,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+
+                vim.api.nvim_create_autocmd( 'LspDetach', {
+                    group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+                    callback = function(event)
+                        vim.lsp.buf.clear_references()
+                        vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event.buf }
+                    end
+                })
+            end
+
+
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+                callback = function(event)
+                    create_keymaps(event.buf)
+
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+                        hide_highlight(event.buf)
+                    end
+
+                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+                        require('../config/keymaps.lua')('<Leader>th', function()
+                            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                        end, 'Toggle inlay hints')
+                    end
+                end,
+            })
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+        end
     },
     {
         'hrsh7th/nvim-cmp',
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,3 +1,5 @@
+local t_builtin = require('telescope.builtin')
+
 return {
     {
         'nvim-telescope/telescope.nvim',
@@ -5,6 +7,15 @@ return {
             { 'nvim-lua/plenary.nvim' },
             { 'nvim-telescope/telescope-ui-select.nvim' },
             { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+        },
+        keys = {
+            { '<Leader>sh', t_builtin.help_tags, desc = 'Search help' },
+            { '<Leader>sf', t_builtin.find_files, desc = 'Search files' },
+            { '<Leader>sg', t_builtin.live_grep, desc = 'Search by grep' },
+            { '<Leader>sw', t_builtin.grep_string, desc = 'Search word under cursor' },
+            { '<Leader>sr', t_builtin.resume, desc = 'Search resume' },
+            { '<Leader>so', t_builtin.oldfiles, desc = 'Search previously open files' },
+            { '<Leader><Leader>', t_builtin.buffers, desc = 'Search open buffers' },
         },
     },
     {
