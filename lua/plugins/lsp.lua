5dd25803c395c7aa82fb060104812f5687229bf4
Pavel Egorov <nxblnd@desktop.localdomain>
2025-02-06 21:52:39 +0300

    Extract completions into separate file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -131,38 +131,6 @@ return {
         end
     },
     {
-        'hrsh7th/nvim-cmp',
-        enabled = true,
-        dependencies = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'saadparwaiz1/cmp_luasnip',
-            {
-                'L3MON4D3/LuaSnip',
-                dependencies = {
-                    'rafamadriz/friendly-snippets',
-                    config = function()
-                        require('luasnip.loaders.from_vscode').lazy_load()
-                    end,
-                },
-            },
-        },
-        opts = {
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            sources = {
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-                { name = 'buffer' },
-                { name = 'path' },
-            },
-        },
-    },
-    {
         'stevearc/conform.nvim',
         event = { 'BufWritePre' },
         cmd = { 'ConformInfo' },
