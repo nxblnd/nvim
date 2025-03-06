2c6ae3e2240bbf8c0b5b7e8731855666fe8dce4d
Pavel Egorov <nxblnd@desktop.localdomain>
2025-03-07 01:50:47 +0300

    Add lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -155,5 +155,17 @@ return {
     {
         "mfussenegger/nvim-ansible",
     },
+    {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+            library = {
+                {
+                    path = '${3rd}/luv/library',
+                    words = { 'vim%.uv' },
+                },
+            },
+        },
+    },
 }
 
