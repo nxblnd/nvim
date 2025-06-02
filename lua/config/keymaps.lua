0f976d3045deab122e83ca0795e4c79a0442dbe9
Pavel Egorov <contact@pavelegorov.xyz>
2025-06-02 22:19:25 +0300

    Change capitalization for <C-> and <Leader>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,18 +6,18 @@ keymap('<space>x', ':.lua<cr>', 'Run current line as lua script')
 keymap('<space>x', ':lua<cr>', 'Run currect selection as lua script', { mode = 'v' })
 
 -- Better 'o' and 'O'
-keymap('<Leader>o', 'o<Esc>o', 'Add 2 lines below and go into Insert mode')
-keymap('<Leader>O', 'O<Esc>O', 'Add 2 lines above and go into Insert mode')
+keymap('<leader>o', 'o<Esc>o', 'Add 2 lines below and go into Insert mode')
+keymap('<leader>O', 'O<Esc>O', 'Add 2 lines above and go into Insert mode')
 
 -- Buffer navigation keymaps
-keymap('<Leader>h', ':bnext<cr>', 'Go to next buffer')
-keymap('<Leader>l', ':bprevious<cr>', 'Go to previous buffer')
+keymap('<leader>h', ':bnext<cr>', 'Go to next buffer')
+keymap('<leader>l', ':bprevious<cr>', 'Go to previous buffer')
 
 -- Split navigation keymaps
-keymap('<C-h>', '<C-w><C-h>', 'Move to left split')
-keymap('<C-j>', '<C-w><C-j>', 'Move to lower split')
-keymap('<C-k>', '<C-w><C-k>', 'Move to upper split')
-keymap('<C-l>', '<C-w><C-l>', 'Move to right split')
+keymap('<c-h>', '<c-w><c-h>', 'Move to left split')
+keymap('<c-j>', '<c-w><c-j>', 'Move to lower split')
+keymap('<c-k>', '<c-w><c-k>', 'Move to upper split')
+keymap('<c-l>', '<c-w><c-l>', 'Move to right split')
 
 -- Split resize keymaps
 keymap('<c-H>', '<c-W><', 'Decrease split columns')
@@ -26,5 +26,5 @@ keymap('<c-K>', '<c-W>+', 'Increase split rows')
 keymap('<c-L>', '<c-W>>', 'Increase split columns')
 
 -- Misc keymaps
-keymap('<Esc><Esc>', '<C-\\><C-n>', 'Exit terminal mode', { mode = 't' })
+keymap('<Esc><Esc>', '<c-\\><c-n>', 'Exit terminal mode', { mode = 't' })
 keymap('<leader>m', ':Oil<cr>', 'Open file manager')
