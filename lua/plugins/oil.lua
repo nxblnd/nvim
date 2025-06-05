a29f8b3526f423b3f007398a13df2b72065da1fe
Pavel Egorov <contact@pavelegorov.xyz>
2025-06-05 22:54:53 +0300

    Tweak oil settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,8 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     lazy = false,
+    opts = {
+        skip_confirm_for_simple_edits = true,
+    },
 }
