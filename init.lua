556dd19ecc475eb428e91b586c853dfff9a2d8b0
Pavel Egorov <contact@pavelegorov.xyz>
2025-06-08 19:43:08 +0300

    Extract lazy into separate file

--- init.lua
+++ init.lua
@@ -2,29 +2,9 @@ require('config.options')
 require('config.variables')
 require('config.keymaps')
 require('config.autocmd')
-
-local lazypath = vim.fn.stdpath('data') .. 'lazy/lazy.nvim'
-
-local function check_lazy_install()
-    if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        vim.fn.system({
-            'git',
-            'clone',
-            '--filter=blob:none',
-            'https://github.com/folke/lazy.nvim.git',
-            '--branch=stable',
-            lazypath,
-        })
-    end
-end
+require('config.lazy')
 
 local function main()
-    check_lazy_install()
-
-    vim.opt.rtp:prepend(lazypath)
-
-    require('lazy').setup('plugins')
-
     vim.cmd.colorscheme('tokyonight')
 end
 
