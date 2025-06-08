56e774d2b2f5945247b0acaf6f9906ab1583d740
Pavel Egorov <contact@pavelegorov.xyz>
2025-06-08 19:54:26 +0300

    Update blink keymaps

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,9 +3,22 @@ return {
     version = '1.*',
     dependencies = { 'rafamadriz/friendly-snippets' },
     opts = {
-        keymap = { preset = 'enter' },
+        keymap = {
+            preset = 'enter',
+            ['<tab>'] = { 'select_next', 'fallback' },
+            ['<s-tab>'] = { 'select_prev', 'fallback' },
+        },
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+            },
+            ghost_text = {
+                enabled = true,
+            },
         },
     },
     opts_extend = { "sources.default" },
